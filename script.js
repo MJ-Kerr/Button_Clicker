730553fccd81606884233b3ed9feb99d86ba0fab
Miles Kerr <m.kerr.professional@gmail.com>
2023-03-21 17:27:10 -0500

    bettter now

--- script.js
+++
@@ -1,9 +0,0 @@
-function thumbDown(jokeSpan) {
-    jokeSpan.innerText--;
-}
-
-function thumbUp(jokeSpan) {
-    jokeSpan.innerText++;
-    alert('ninja has been liked')
-}
-
---
+++ source.js
@@ -0,0 +1,10 @@
+function showAlert() {
+    alert("Ninja was liked!");
+}
+
+const btn = document.getElementById('btn');
+
+
+btn.addEventListener('click', function handleClick() {
+    btn.textContent = 'Logout';
+});
